7fc6d2305f55eaef116a18bca48252f16d14d10e
CodeJ00 <66993701+CodeJ00@users.noreply.github.com>
2021-12-16 21:16:50 +0800

    pull commit

--- src/main/java/com/mg/GitTest.java
+++ src/main/java/com/mg/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("master");
         System.out.println("Hello git3 hot-fix!");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
