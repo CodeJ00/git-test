d78681abd30f802083dec84e2d9d7924ebe9c52d
lmj <lmj@gmail.com>
2021-12-16 20:16:30 +0800

    first commit

---
+++ src/main/java/com/mg/GitTest.java
@@ -0,0 +1,13 @@
+package com.mg;
+
+/**
+ * @author 37714
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/12/16 20:11:53
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
